dfbf55b082c8686ea8ee385f5892b365263fdcc4
Andreas Lappe <nd@off-pist.de>
2009-07-27 14:48:29 +0000

    Initial upload
    git-svn-id: https://zak.mine.nu/svn/hyphenator/trunk@12 d2392733-152b-4ac1-9dd5-2c7832ab9186

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "hyphenator"
 #
-# Auto generated 24-07-2009 18:13
+# Auto generated 27-07-2009 16:04
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Uses hyphenator JavaScript to properly justified paragraphs (Hyphenator 2.3.0)',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.0.1',
+	'version' => '0.0.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:29:{s:23:"class.tx_hyphenator.php";s:4:"1df0";s:17:"ext_localconf.php";s:4:"8003";s:24:"ext_typoscript_setup.txt";s:4:"8d6a";s:17:"res/Hyphenator.js";s:4:"cf95";s:21:"res/Version 2.3.0.zip";s:4:"1b3d";s:18:"res/patterns/bn.js";s:4:"83f0";s:18:"res/patterns/cs.js";s:4:"587f";s:18:"res/patterns/da.js";s:4:"5982";s:18:"res/patterns/de.js";s:4:"4c45";s:18:"res/patterns/en.js";s:4:"d5d4";s:18:"res/patterns/es.js";s:4:"f031";s:18:"res/patterns/fi.js";s:4:"f89c";s:18:"res/patterns/fr.js";s:4:"1e88";s:18:"res/patterns/gu.js";s:4:"e698";s:18:"res/patterns/hi.js";s:4:"3c85";s:18:"res/patterns/hu.js";s:4:"3864";s:18:"res/patterns/it.js";s:4:"3f60";s:18:"res/patterns/kn.js";s:4:"aa76";s:18:"res/patterns/ml.js";s:4:"ba43";s:18:"res/patterns/nl.js";s:4:"2819";s:18:"res/patterns/or.js";s:4:"9514";s:18:"res/patterns/pa.js";s:4:"3aba";s:18:"res/patterns/pl.js";s:4:"1a6d";s:18:"res/patterns/pt.js";s:4:"79b6";s:18:"res/patterns/ru.js";s:4:"00ed";s:18:"res/patterns/sv.js";s:4:"4f83";s:18:"res/patterns/ta.js";s:4:"696d";s:18:"res/patterns/te.js";s:4:"04f9";s:18:"res/patterns/uk.js";s:4:"cd7f";}',
+	'_md5_values_when_last_written' => 'a:32:{s:23:"class.tx_hyphenator.php";s:4:"41b1";s:12:"ext_icon.gif";s:4:"fa27";s:12:"ext_icon.png";s:4:"1f27";s:17:"ext_localconf.php";s:4:"8003";s:24:"ext_typoscript_setup.txt";s:4:"e265";s:14:"doc/manual.sxw";s:4:"34b5";s:17:"res/Hyphenator.js";s:4:"cf95";s:21:"res/Version 2.3.0.zip";s:4:"1b3d";s:18:"res/patterns/bn.js";s:4:"83f0";s:18:"res/patterns/cs.js";s:4:"587f";s:18:"res/patterns/da.js";s:4:"5982";s:18:"res/patterns/de.js";s:4:"4c45";s:18:"res/patterns/en.js";s:4:"d5d4";s:18:"res/patterns/es.js";s:4:"f031";s:18:"res/patterns/fi.js";s:4:"f89c";s:18:"res/patterns/fr.js";s:4:"1e88";s:18:"res/patterns/gu.js";s:4:"e698";s:18:"res/patterns/hi.js";s:4:"3c85";s:18:"res/patterns/hu.js";s:4:"3864";s:18:"res/patterns/it.js";s:4:"3f60";s:18:"res/patterns/kn.js";s:4:"aa76";s:18:"res/patterns/ml.js";s:4:"ba43";s:18:"res/patterns/nl.js";s:4:"2819";s:18:"res/patterns/or.js";s:4:"9514";s:18:"res/patterns/pa.js";s:4:"3aba";s:18:"res/patterns/pl.js";s:4:"1a6d";s:18:"res/patterns/pt.js";s:4:"79b6";s:18:"res/patterns/ru.js";s:4:"00ed";s:18:"res/patterns/sv.js";s:4:"4f83";s:18:"res/patterns/ta.js";s:4:"696d";s:18:"res/patterns/te.js";s:4:"04f9";s:18:"res/patterns/uk.js";s:4:"cd7f";}',
 	'suggests' => array(
 	),
 );
