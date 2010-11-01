cfaabf3304dab9004eb5c47caca5c83df639cb71
Andreas Lappe <nd@off-pist.de>
2010-11-01 10:47:10 +0100

    [~TASK] Some cleanups...

--- Classes/Hyphenator.php
+++ Classes/Hyphenator.php
@@ -1,26 +1,26 @@
 <?php
 
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2009     Andreas Lappe <nd@off-pist.de>
-*
-*  All rights reserved
-*
-*  This script is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; version 2 of the License.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2009     Andreas Lappe <nd@off-pist.de>
+ *
+ *  All rights reserved
+ *
+ *  This script is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * Hyphenator
@@ -53,7 +53,7 @@ class tx_hyphenator {
 
 		// Set config global
 		$this->config = &$params['pObj']->config['config']['tx_hyphenator.'];
-		
+
 		// Build config string from it
 		if(count($this->config) > 0) {
 			$this->configstring = $this->handleConfig();
@@ -71,7 +71,7 @@ class tx_hyphenator {
 	 */
 	public function process($content){
 		// Add the tracking code to the end of <head> element
-		$content = str_replace('</body>', $this->generateScriptCcode( $pageName ).'</body>', $content);
+		$content = str_replace('</body>', $this->generateScriptCode( $pageName ).'</body>', $content);
 		return $content;
 	}
 
@@ -95,53 +95,41 @@ class tx_hyphenator {
 		$configArray;
 
 		if(isset($this->config['classname'])) {
-			$configArray[] = 'classname : \''.$this->config['classname'].'\'';
+			$configArray[] = 'classname: \''.$this->config['classname'].'\'';
 		}
 
 		if(isset($this->config['donthyphenateclassname'])) {
-			$configArray[] = 'donthyphenateclassname : \''.$this->config['donthyphenateclassname'].'\'';
+			$configArray[] = 'donthyphenateclassname: \''.$this->config['donthyphenateclassname'].'\'';
 		}
 
 		if(isset($this->config['displaytogglebox'])) {
-			if($this->config['displaytogglebox']) {
-				$configArray[] = 'displaytogglebox : true';
-			} else {
-				$configArray[] = 'displaytogglebox : false';
-			}
+			$configArray[] = ($this->config['displaytogglebox']) ? 'displaytogglebox: true' : 'displaytogglebox: false';
 		}
 
 		if(isset($this->config['minwordlength'])) {
-			$configArray[] = 'minwordlength : '.$this->config['minwordlength'];
+			$configArray[] = 'minwordlength: '.$this->config['minwordlength'];
 		}
 
 		if(isset($this->config['remoteloading'])) {
-			if($this->config['remoteloading']) {
-				$configArray[] = 'remoteloading : true';
-			} else {
-				$configArray[] = 'remoteloading : false';
-			}
+			$configArray[] = ($this->config['remoteloading']) ? 'remoteloading: true' : 'remoteloading: false';
 		}
-		
+
 		if(isset($this->config['enablecache'])) {
-			if($this->config['enablecache']) {
-				$configArray[] = 'enablecache : true';
-			} else {
-				$configArray[] = 'enablecache : false';
-			}
+			$configArray[] = ($this->config['enablecache']) ? 'enablecache: true' : 'enablecache: false';
 		}
 
 		if(isset($this->config['intermediatestate'])) {
-			$configArray[] = 'intermediatestate : \''.$this->config['intermediatestate'].'\'';
+			$configArray[] = 'intermediatestate: \''.$this->config['intermediatestate'].'\'';
 		}
 
 		if(isset($this->config['onerrorhandler'])) {
-			$configArray[] = 'onerrorhandler : '.$this->config['onerrorhandler'];
+			$configArray[] = 'onerrorhandler: '.$this->config['onerrorhandler'];
 		}
 
 		// done
 		return 'Hyphenator.config({'.implode(', ', $configArray).'});';
 	}
-	
+
 }
 
 if (defined("TYPO3_MODE") && $TYPO3_CONF_VARS[TYPO3_MODE]["XCLASS"]["ext/hyphenator/class.tx_hyphenator.php"])	{
