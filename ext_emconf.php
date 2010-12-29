3f9ad34be49df0d870ed00ab961e59e099494952
Andreas Lappe <nd@off-pist.de>
2010-12-29 14:13:52 +0100

    [~TASK] Updated em-conf by uploading to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "hyphenator".
 #
-# Auto generated 01-11-2010 10:51
+# Auto generated 29-12-2010 14:13
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Uses hyphenator JavaScript to properly justified paragraphs.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:42:{s:12:"ext_icon.gif";s:4:"fa27";s:12:"ext_icon.png";s:4:"1f27";s:17:"ext_localconf.php";s:4:"b4d3";s:24:"ext_typoscript_setup.txt";s:4:"aecf";s:22:"Classes/Hyphenator.php";s:4:"c000";s:34:"Resources/JavaScript/Hyphenator.js";s:4:"bd67";s:35:"Resources/JavaScript/patterns/be.js";s:4:"c548";s:35:"Resources/JavaScript/patterns/bn.js";s:4:"0eb4";s:35:"Resources/JavaScript/patterns/cs.js";s:4:"2944";s:35:"Resources/JavaScript/patterns/da.js";s:4:"18c5";s:35:"Resources/JavaScript/patterns/de.js";s:4:"6f04";s:43:"Resources/JavaScript/patterns/el-monoton.js";s:4:"a012";s:43:"Resources/JavaScript/patterns/el-polyton.js";s:4:"8a5e";s:38:"Resources/JavaScript/patterns/en-gb.js";s:4:"72b3";s:38:"Resources/JavaScript/patterns/en-us.js";s:4:"7824";s:35:"Resources/JavaScript/patterns/en.js";s:4:"5b54";s:35:"Resources/JavaScript/patterns/es.js";s:4:"2855";s:35:"Resources/JavaScript/patterns/fi.js";s:4:"40be";s:35:"Resources/JavaScript/patterns/fr.js";s:4:"df96";s:36:"Resources/JavaScript/patterns/grc.js";s:4:"4764";s:35:"Resources/JavaScript/patterns/gu.js";s:4:"3b37";s:35:"Resources/JavaScript/patterns/hi.js";s:4:"45a3";s:35:"Resources/JavaScript/patterns/hu.js";s:4:"c25d";s:35:"Resources/JavaScript/patterns/hy.js";s:4:"4896";s:35:"Resources/JavaScript/patterns/it.js";s:4:"6630";s:35:"Resources/JavaScript/patterns/kn.js";s:4:"7488";s:35:"Resources/JavaScript/patterns/la.js";s:4:"1b16";s:35:"Resources/JavaScript/patterns/lt.js";s:4:"9233";s:35:"Resources/JavaScript/patterns/ml.js";s:4:"6b03";s:35:"Resources/JavaScript/patterns/nl.js";s:4:"9bcf";s:35:"Resources/JavaScript/patterns/or.js";s:4:"69ce";s:35:"Resources/JavaScript/patterns/pa.js";s:4:"6e91";s:35:"Resources/JavaScript/patterns/pl.js";s:4:"42bd";s:35:"Resources/JavaScript/patterns/pt.js";s:4:"4a37";s:35:"Resources/JavaScript/patterns/ru.js";s:4:"9eef";s:35:"Resources/JavaScript/patterns/sl.js";s:4:"d772";s:35:"Resources/JavaScript/patterns/sv.js";s:4:"10eb";s:35:"Resources/JavaScript/patterns/ta.js";s:4:"9b63";s:35:"Resources/JavaScript/patterns/te.js";s:4:"5a05";s:35:"Resources/JavaScript/patterns/tr.js";s:4:"2478";s:35:"Resources/JavaScript/patterns/uk.js";s:4:"3103";s:14:"doc/manual.sxw";s:4:"071e";}',
+	'_md5_values_when_last_written' => 'a:44:{s:12:"ext_icon.gif";s:4:"fa27";s:12:"ext_icon.png";s:4:"1f27";s:17:"ext_localconf.php";s:4:"b4d3";s:24:"ext_typoscript_setup.txt";s:4:"aecf";s:22:"Classes/Hyphenator.php";s:4:"c000";s:34:"Resources/JavaScript/Hyphenator.js";s:4:"5f86";s:35:"Resources/JavaScript/patterns/be.js";s:4:"c548";s:35:"Resources/JavaScript/patterns/bn.js";s:4:"0eb4";s:35:"Resources/JavaScript/patterns/cs.js";s:4:"2944";s:35:"Resources/JavaScript/patterns/da.js";s:4:"18c5";s:35:"Resources/JavaScript/patterns/de.js";s:4:"6f04";s:43:"Resources/JavaScript/patterns/el-monoton.js";s:4:"a012";s:43:"Resources/JavaScript/patterns/el-polyton.js";s:4:"8a5e";s:38:"Resources/JavaScript/patterns/en-gb.js";s:4:"72b3";s:38:"Resources/JavaScript/patterns/en-us.js";s:4:"d6ff";s:35:"Resources/JavaScript/patterns/en.js";s:4:"5b54";s:35:"Resources/JavaScript/patterns/es.js";s:4:"2855";s:35:"Resources/JavaScript/patterns/fi.js";s:4:"40be";s:35:"Resources/JavaScript/patterns/fr.js";s:4:"df96";s:36:"Resources/JavaScript/patterns/grc.js";s:4:"4764";s:35:"Resources/JavaScript/patterns/gu.js";s:4:"3b37";s:35:"Resources/JavaScript/patterns/hi.js";s:4:"45a3";s:35:"Resources/JavaScript/patterns/hu.js";s:4:"c25d";s:35:"Resources/JavaScript/patterns/hy.js";s:4:"4896";s:35:"Resources/JavaScript/patterns/it.js";s:4:"6630";s:35:"Resources/JavaScript/patterns/kn.js";s:4:"7488";s:35:"Resources/JavaScript/patterns/la.js";s:4:"1b16";s:35:"Resources/JavaScript/patterns/lt.js";s:4:"9233";s:35:"Resources/JavaScript/patterns/lv.js";s:4:"61ac";s:35:"Resources/JavaScript/patterns/ml.js";s:4:"6b03";s:35:"Resources/JavaScript/patterns/nl.js";s:4:"9bcf";s:38:"Resources/JavaScript/patterns/no-nb.js";s:4:"1c4c";s:35:"Resources/JavaScript/patterns/or.js";s:4:"69ce";s:35:"Resources/JavaScript/patterns/pa.js";s:4:"6e91";s:35:"Resources/JavaScript/patterns/pl.js";s:4:"42bd";s:35:"Resources/JavaScript/patterns/pt.js";s:4:"4a37";s:35:"Resources/JavaScript/patterns/ru.js";s:4:"9eef";s:35:"Resources/JavaScript/patterns/sl.js";s:4:"d772";s:35:"Resources/JavaScript/patterns/sv.js";s:4:"10eb";s:35:"Resources/JavaScript/patterns/ta.js";s:4:"9b63";s:35:"Resources/JavaScript/patterns/te.js";s:4:"5a05";s:35:"Resources/JavaScript/patterns/tr.js";s:4:"2478";s:35:"Resources/JavaScript/patterns/uk.js";s:4:"3103";s:14:"doc/manual.sxw";s:4:"071e";}',
 	'suggests' => array(
 	),
 );
